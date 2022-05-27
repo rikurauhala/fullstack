8a2ddf1f6d15fff28283d195fc18fe7bb540b3b3
Riku Rauhala <riku@rauhala.io>
2022-05-27 11:24:21 +0300

    Complete exercise 2.9*

--- exercises/part02/phonebook/src/App.js
+++ exercises/part02/phonebook/src/App.js
@@ -2,7 +2,10 @@ import { useState } from 'react'
 
 const Person = ({ name, number }) => {
   return (
-    <li>{name} {number}</li>
+    <tr>
+      <td>{name}</td>
+      <td>{number}</td>
+    </tr>
   )
 }
 
@@ -10,9 +13,15 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [persons, setPersons] = useState([
-    { name: 'Mickey Mouse', number: 1313 },
-    { name: 'Donald Duck', number: 313 },
+    { name: 'Mickey Mouse', number: "123-456789" },
+    { name: 'Donald Duck', number: "313-313313" },
+    { name: 'Scrooge McDuck', number: "999-999999" },
   ])
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const searchResults = searchTerm.length > 0
+    ? persons.filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    : persons
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -30,6 +39,8 @@ const App = () => {
     }
   }
 
+  const handleSearchChange = (event) => {setSearchTerm(event.target.value)}
+
   const handleNameChange = (event) => setNewName(event.target.value)
 
   const handleNumberChange = (event) => setNewNumber(event.target.value)
@@ -37,17 +48,62 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <h3>Add new person</h3>
       <form onSubmit={addPerson}>
-        <div>Name: <input value={newName} onChange={handleNameChange} /></div>
-        <div>Number: <input value={newNumber} onChange={handleNumberChange} /></div>
-        <div><button type="submit">+ Add</button></div>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                Name
+              </td>
+              <td>
+                <input value={newName} onChange={handleNameChange} />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Number
+              </td>
+              <td>
+                <input value={newNumber} onChange={handleNumberChange} />
+              </td>
+              <td>
+                <button type="submit">+ Add</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map(person => 
-          <Person key={person.name} name={person.name} number={person.number} />
-        )}
-      </ul>
+      <h3>Numbers</h3>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              Search
+            </td>
+            <td>
+              <input value={searchTerm} onChange={handleSearchChange} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              Name
+            </th>
+            <th>
+              Number
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {searchResults.map(person => 
+            <Person key={person.name} name={person.name} number={person.number} />
+          )}
+        </tbody>
+      </table>
     </div>
   )
 }

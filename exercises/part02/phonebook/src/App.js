6d1a1c0fbf4e6cb4f9d26023e616c718f7099846
Riku Rauhala <riku@rauhala.io>
2022-05-28 21:44:22 +0300

    Complete exercise 2.15

--- exercises/part02/phonebook/src/App.js
+++ exercises/part02/phonebook/src/App.js
@@ -32,9 +32,13 @@ const App = () => {
         name: newName,
         number: newNumber
       }
-      setPersons(persons.concat(person))
-      setNewName('')
-      setNewNumber('')
+      axios
+        .post('http://localhost:3001/persons', person)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+        })
     }
   }
 

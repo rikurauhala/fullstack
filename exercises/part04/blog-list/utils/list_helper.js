568ef493eae59642d6118d4da78bf6fc45feb020
Riku Rauhala <riku@rauhala.io>
2022-06-08 23:41:40 +0300

    Complete exercise 4.4

--- exercises/part04/blog-list/utils/list_helper.js
+++ exercises/part04/blog-list/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+
+  const reducer = (sum, blog) => sum + blog.likes
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }

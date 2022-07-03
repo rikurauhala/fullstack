94f6aad646f4c7d5ed53bde804d8f58e1893929e
Riku Rauhala <riku@rauhala.io>
2022-07-03 11:57:44 +0300

    Add LoginForm

--- exercises/part05/bloglist-frontend/src/App.js
+++ exercises/part05/bloglist-frontend/src/App.js
@@ -1,19 +1,36 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
+      setBlogs(blogs)
     )  
   }, [])
 
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('Logging in with', username, password)
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
+      <h2>Blogs</h2>
+
+      <LoginForm 
+        handleLogin={handleLogin}
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+      />
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
---
+++ exercises/part05/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,43 @@
+const PersonForm = ({ handleLogin, username, setUsername, password, setPassword }) => {
+  return (
+    <form onSubmit={handleLogin}>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              Username
+            </td>
+            <td>
+              <input
+                name="Username"
+                onChange={({ target }) => setUsername(target.value)}
+                placeholder='Username'
+                type="text"
+                value={username}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Password
+            </td>
+            <td>
+              <input
+                name="Password"
+                onChange={({ target }) => setPassword(target.value)}
+                placeholder='Password'
+                type="password"
+                value={password}
+              />
+            </td>
+            <td>
+              <button type="submit">Login</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+  )
+}
+  
+export default PersonForm

125626bb8f2198fbd503aeb62af2a76719862454
Riku Rauhala <riku@rauhala.io>
2022-07-06 18:02:49 +0300

    Complete exercise 5.7*

--- exercises/part05/bloglist-frontend/src/components/Blog.js
+++ exercises/part05/bloglist-frontend/src/components/Blog.js
@@ -1,15 +1,49 @@
-const Blog = ({ blog }) => (
-  <tr>
-    <td>
-      {blog.title}
-    </td>
-    <td>
-      {blog.author}
-    </td>
-    <td>
-      {blog.url}
-    </td>
-  </tr>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  if (!visible) {
+    return (
+      <tr>
+        <td>
+          <button onClick={() => setVisible(true)}>
+            View
+          </button>
+        </td>
+        <td>
+          {blog.title}
+        </td>
+      </tr>
+    )
+  }
+
+  return (
+    <tr>
+      <td>
+          <button onClick={() => setVisible(false)}>
+            Hide
+          </button>
+        </td>
+      <td>
+        {blog.title}
+      </td>
+      <td>
+        {blog.author}
+      </td>
+      <td>
+        {blog.url}
+      </td>
+      <td>
+        {blog.likes} {blog.likes === 1 ? "like" : "likes"}
+      </td>
+      <td>
+        <button>
+          +
+        </button>
+      </td>
+    </tr>
+  )
+}
 
 export default Blog
--- exercises/part05/bloglist-frontend/src/components/Blogs.js
+++ exercises/part05/bloglist-frontend/src/components/Blogs.js
@@ -2,25 +2,6 @@ import Blog from './Blog'
 
 const Blogs = ({ blogs }) => (
   <table>
-    <thead>
-      <tr>
-        <td>
-          <b>
-            Title
-          </b>
-        </td>
-        <td>
-          <b>
-            Author
-          </b>
-        </td>
-        <td>
-          <b>
-            URL
-          </b>
-        </td>
-      </tr>
-    </thead>
     <tbody>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />

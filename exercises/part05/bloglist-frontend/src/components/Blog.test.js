e7c5731b47b294ec991b0950799b8b6d09e06606
Riku Rauhala <riku@rauhala.io>
2022-07-09 21:55:46 +0300

    Complete exercise 5.14

--- exercises/part05/bloglist-frontend/src/components/Blog.test.js
+++ exercises/part05/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 const title = 'Blog title'
@@ -35,5 +36,30 @@ describe('Correct elements are rendered by default', () => {
 
   test('Likes are not rendered', () => {
     expect(container).not.toHaveTextContent(likes)
+  })
+})
+
+describe('Correct elements are rendered after clicking the View button', () => {
+  beforeEach(async () => {
+    render(<Blog blog={blog}/>)
+    const button = screen.getByText('+ View')
+    const user = userEvent.setup()
+    await user.click(button)
+  })
+
+  test('Title is rendered', () => {
+    screen.getByText(title)
+  })
+
+  test('Author is rendered', () => {
+    screen.getByText(author)
+  })
+
+  test('Url is rendered', () => {
+    screen.getByText(url)
+  })
+
+  test('Likes are rendered', () => {
+    screen.getByText(`${likes} likes`)
   })
 })

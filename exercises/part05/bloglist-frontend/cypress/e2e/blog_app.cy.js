6fe7379a124b759a7944e136d84c6c455a0a404d
Riku Rauhala <riku@rauhala.io>
2022-07-10 19:00:41 +0300

    Complete exercise 5.18

--- exercises/part05/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ exercises/part05/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,18 @@
-const frontendUrl = 'http://localhost:3000'
-const resetUrl = 'http://localhost:3003/api/testing/reset'
+const frontend = 'http://localhost:3000'
+const resetUrl = `${frontend}/api/testing/reset`
+const usersUrl = `${frontend}/api/users`
+
+const user = {
+  name: 'User',
+  username: 'User',
+  password: 'password'
+}
 
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', resetUrl)
-    cy.visit(frontendUrl)
+    cy.request('POST', usersUrl, user)
+    cy.visit(frontend)
   })
 
   it('Login form is shown', function() {
@@ -14,5 +22,24 @@ describe('Blog app', function() {
     cy.contains('Password')
     cy.get('#password')
     cy.contains('Log in')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type(user.username)
+      cy.get('#password').type(user.password)
+      cy.get('#login-button').click()
+      cy.contains('Welcome to Bloglist!')
+      cy.contains(`Logged in as ${user.name}`)
+      cy.contains('Log out')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type(user.username)
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+      cy.contains('Failed to log in!')
+      cy.contains('Login')
+    })
   })
 })

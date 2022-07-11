06b479ba6244a1f85d18c27867d961163f64016f
Riku Rauhala <riku@rauhala.io>
2022-07-11 20:21:38 +0300

    Complete exercise 6.2

--- exercises/part06/unicafe-redux/src/index.js
+++ exercises/part06/unicafe-redux/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 import { createStore } from 'redux'
 import reducer from './reducer'
+import './index.css'
 
 const store = createStore(reducer)
 
@@ -12,15 +13,41 @@ const App = () => {
     })
   }
 
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+
+  const reset = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
   return (
     <div>
-      <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={good}>
+        good
+      </button>
+      <button onClick={ok}>
+        ok
+      </button>
+      <button onClick={bad}>
+        bad
+      </button>
+      <button onClick={reset}>
+        reset stats
+      </button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }

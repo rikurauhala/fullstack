e0cbc3b043b5f36a3b7af8b4f58f19e3af246cde
Riku Rauhala <riku@rauhala.io>
2022-07-30 11:15:24 +0300

    Complete exercise 9.9

--- exercises/part09/patientor/backend/index.ts
+++ exercises/part09/patientor/backend/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 const app = express();
 app.use(express.json());
+const cors = require('cors');
+app.use(cors())
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('Ping received!');

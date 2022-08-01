78a486c2f45cda63d75a2dc8ab5db594add1f648
Riku Rauhala <riku@rauhala.io>
2022-08-01 21:05:34 +0300

    Update the type CoursePart

--- exercises/part09/course-information/src/types.ts
+++ exercises/part09/course-information/src/types.ts
@@ -2,3 +2,27 @@ export interface Part {
   name: string,
   exerciseCount: number
 }
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CourseNormalPart extends CoursePartBase {
+  type: "normal";
+  description: string;
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartBase {
+  type: "submission";
+  description: string;
+  exerciseSubmissionLink: string;
+}
+
+export type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;

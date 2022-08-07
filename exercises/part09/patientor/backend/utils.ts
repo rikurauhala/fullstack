dbb17d8c54e32d3c758a468eca24635d5e5d1aa8
Riku Rauhala <riku@rauhala.io>
2022-08-07 17:43:21 +0300

    Refactor utils.ts

--- exercises/part09/patientor/backend/utils.ts
+++ exercises/part09/patientor/backend/utils.ts
@@ -1,5 +1,15 @@
 import { Fields, Gender, NewPatient } from './types';
 
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date));
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isGender = (param: any): param is Gender => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  return Object.values(Gender).includes(param);
+};
+
 const isString = (text: unknown): text is string => {
   return typeof text === 'string' || text instanceof String;
 };
@@ -21,23 +31,12 @@ const parseOccupation = (occupation: unknown): string => {
   return occupation;
 };
 
-const isDate = (date: string): boolean => {
-  return Boolean(Date.parse(date));
-};
-
-const parseDateOfBirth = (date: unknown): string => {
+const parseDate = (date: unknown): string => {
   if (!date || !isString(date) || !isDate(date)) {
     throw new Error('Incorrect or missing date: ' + date);
   }
   return date;
 };
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const isGender = (param: any): param is Gender => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  return Object.values(Gender).includes(param);
-};
-
 
 const parseGender = (gender: unknown): Gender => {
   if (!gender || !isGender(gender)) {
@@ -49,7 +48,7 @@ const parseGender = (gender: unknown): Gender => {
 const toNewPatient = ({ name, dateOfBirth, ssn, gender, occupation }: Fields): NewPatient => {
   const newPatient: NewPatient = {
     name: parseName(name),
-    dateOfBirth: parseDateOfBirth(dateOfBirth),
+    dateOfBirth: parseDate(dateOfBirth),
     ssn: parseSSN(ssn),
     gender: parseGender(gender),
     occupation: parseOccupation(occupation),
